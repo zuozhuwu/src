f3678de3dac641798df0686a5d1e04e2245377e5
zuozhuwu <zuozhuwu@gmail.com>
2022-04-02 15:32:47 +0800

    'update'

--- Time/main.cpp
+++ Time/main.cpp
@@ -1,33 +1,24 @@
-#include <iostream>
-#include <vector>
-#include <ctime>
-#include <string>
 #include "db/Time.h"
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <vector>
+
 using namespace std;
 
+int main() {
+	// Time t1("2022-05-01");
+	// cout<<t1.getStrTime()<<endl;
 
-int main()
-{
-    // time_t now = time(0);
+	// cout<<Time::getTimeFormat()<<endl;
+	// Time::setTimeFormat("MM/DD/YYYY hh:mm:ss");
 
-    // cout << "1970 secs:" << now << endl;
+	// t1.addHour(2);
+	// cout<<t1.getStrTime()<<endl;
+	// cout<<t1.getIntTime()<<endl;
 
-    // struct tm  ltm;
-    // localtime_s(&ltm,&now);
-    Time t1("2022-05-01");
-    cout<<t1.getStrTime()<<endl;
+	
+	
 
-    cout<<Time::getTimeFormat()<<endl;
-    Time::setTimeFormat("MM/DD/YYYY hh:mm:ss");
-
-
-    t1.addHour(2);
-    cout<<t1.getStrTime()<<endl;
-    cout<<t1.getIntTime()<<endl;
-
-
-    
-
-
-    return 0;
+	return 0;
 }
